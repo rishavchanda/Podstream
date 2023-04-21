b935239dcdc03e452d529b96ffbc7454d6a59858
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-04-22 01:28:47 +0530

    added authentication signin signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import { ThemeProvider } from "styled-components";
 import { useState } from "react";
-import {darkTheme, lightTheme} from './utils/Themes.js'
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import Signup from '../src/components/Signup.jsx';
+import Signin from '../src/components/Signin.jsx';
+import OTP from '../src/components/OTP.jsx'
 
 function App() {
 
   const [darkMode, setDarkMode] = useState(true);
+  const [SignUpOpen, setSignUpOpen] = useState(false);
+  const [SignInOpen, setSignInOpen] = useState(true);
 
 
   return (
 
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <>Start</>
+      
     </ThemeProvider>
   );
 }
