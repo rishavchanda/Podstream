c91e8c5a5926c3ef91b462093f4e6a6ce9b9136a
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-04-22 20:48:53 +0530

    most popular podcasts map

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { getMostPopularPodcast } from '../api';
+import { getMostPopularPodcast } from '../api/index';
 import { getPodcastByCategory } from '../api';
+import { PodcastCard } from '../components/PodcastCard.jsx'
 
 const DashboardMain = styled.div`
 padding: 30px 0px;
@@ -12,42 +13,61 @@ padding: 24px;
 box-sizing: border-box;
 height: 250px;
 background-color: ${({ theme }) => theme.bg};
+display: flex;
+flex-direction: column;
 `;
 const Topic = styled.div`
   color: ${({ theme }) => theme.text_primary};
   font-size: 24px;
 `;
 const Podcasts = styled.div`
-  
+  display: flex;
+  flex-direction: row;
+  gap: 18px;
 `;
 
 const Dashboard = () => {
 
   const [mostPopular, setMostPopular] = useState([]);
+  const [comedy, setComedy] = useState([1]);
+
+  useEffect(() => {
+    getMostPopularPodcast()
+      .then((res) => {
+        setMostPopular(res.data)
+        console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }, [])
 
   useEffect(()=>{
-    getMostPopularPodcast()
-  .then((res)=>setMostPopular(res))
-  .catch((error)=>console.log(error));
+    getPodcastByCategory("comedy")
+  .then((res)=>{
+    setMostPopular(res.data)
+    console.log(res.data)
   })
+  .catch((error)=>console.log(error));
+  },[])
 
   return (
     <DashboardMain>
       <FilterContainer>
         <Topic>Most Popular</Topic>
         <Podcasts>
-          {mostPopular.map((podcast)=>{
-
-          })}
+          {mostPopular.map((podcast) => (
+            <PodcastCard />
+          ))}
         </Podcasts>
       </FilterContainer>
       <FilterContainer>
-        <Topic>Most Popular</Topic>
-        <Podcasts>
-          {mostPopular.map((podcast)=>{
-            
-          })}
-        </Podcasts>
+        <Topic>Comedy</Topic>
+        {/* <Podcasts>
+          {comedy.map((podcast) => (
+            <PodcastCard />
+          ))}
+        </Podcasts> */}
       </FilterContainer>
     </DashboardMain>
   )
