4e85ce9ce4a98d231836c973485f41773d0df39c
rishavchanda <rishavchanda0@gmail.com>
2023-04-22 17:26:00 +0530

    made the user routes and controler

---
+++ server/controllers/user.js
@@ -0,0 +1,32 @@
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+
+export const update = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json(updatedUser);
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, "You can update only your account!"));
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+      const user = await User.findById(req.user.id).populate("podcasts").populate("favorits");
+      res.status(200).json(user);
+    } catch (err) {
+      console.log(req.user)
+      next(err);
+    }
+  }
