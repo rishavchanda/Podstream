ac979f058235c540c9f06a865256df4ff9848ad6
Aritra37 <muhuriaritra37@gmail.com>
2023-04-22 20:46:24 +0530

    made favourites

--- client/src/pages/Favourites.jsx
+++ client/src/pages/Favourites.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {PodcastCard} from '../components/PodcastCard';
 
+const CardWrapper=styled.div`
+  
+  display:flex;
+  flex-wrap:wrap;
+  gap:20px;
+`
+const FavouritesContainer=styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  font-size:2rem;
+  font-weight:500;
+  margin-bottom:2rem;
+`
+const Container=styled.div`
+  padding:2.5rem;
+`
 const Favourites = () => {
   return (
-    <div>Favourites</div>
+    <div>
+      <Container>
+      <FavouritesContainer>
+          Favourites
+      </FavouritesContainer>
+          <CardWrapper>
+              <PodcastCard/>
+              <PodcastCard/>
+              <PodcastCard/>
+              <PodcastCard/>
+              <PodcastCard/>
+              <PodcastCard/>
+              <PodcastCard/>
+              <PodcastCard/>
+          </CardWrapper>
+        </Container>
+    </div>
   )
 }
 
