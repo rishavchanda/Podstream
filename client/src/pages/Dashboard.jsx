490f5e88d46ea390dbdbd3479fcf6468977d8d72
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-04-22 19:58:03 +0530

    added an most popular api

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import styled from 'styled-components';
+import { getMostPopularPodcast } from '../api';
+import { getPodcastByCategory } from '../api';
 
 const DashboardMain = styled.div`
-padding: 50px 0px;
+padding: 30px 0px;
 `;
 const FilterContainer = styled.div`
-margin:0px 20px ;
+margin:20px 20px ;
 padding: 24px;
 box-sizing: border-box;
-
 height: 250px;
 background-color: ${({ theme }) => theme.bg};
 `;
@@ -16,12 +17,37 @@ const Topic = styled.div`
   color: ${({ theme }) => theme.text_primary};
   font-size: 24px;
 `;
+const Podcasts = styled.div`
+  
+`;
 
 const Dashboard = () => {
+
+  const [mostPopular, setMostPopular] = useState([]);
+
+  useEffect(()=>{
+    getMostPopularPodcast()
+  .then((res)=>setMostPopular(res))
+  .catch((error)=>console.log(error));
+  })
+
   return (
     <DashboardMain>
       <FilterContainer>
         <Topic>Most Popular</Topic>
+        <Podcasts>
+          {mostPopular.map((podcast)=>{
+
+          })}
+        </Podcasts>
+      </FilterContainer>
+      <FilterContainer>
+        <Topic>Most Popular</Topic>
+        <Podcasts>
+          {mostPopular.map((podcast)=>{
+            
+          })}
+        </Podcasts>
       </FilterContainer>
     </DashboardMain>
   )
