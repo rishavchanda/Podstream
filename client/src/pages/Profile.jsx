7cfe46d88a04bdf0f34b59de5d9a1bc012f034f4
Shinjini123 <92155085+Shinjini123@users.noreply.github.com>
2023-04-23 21:07:05 +0530

    made profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import Avatar from '@mui/material/Avatar';
  const  Container=styled.div`
-
+gap:100px;
+display:flex;
+  flex-wrap:wrap;
  `
  const ProfileAvatar=styled.div`
   display:flex;
   flex-wrap:wrap;
-  gap:20px;
+  padding-left:3rem;
+`
+const Container_Profile=styled.div`
 `
 const ProfileName=styled.div`
-display:flex;
-flex-wrap:wrap;
-gap:20px;
+gap:100px;
 color: ${({ theme }) => theme.text_primary};
-font-size:4rem;
-font-weight:1000;
+font-size:3rem;
+font-weight:700;
+`
+const Profile_email=styled.div`
+color:#2b6fc2;
+
+font-size:1.2rem;
+font-weight:bold;
 `
 
 const Profile = () => {
@@ -25,10 +33,17 @@ const Profile = () => {
             <ProfileAvatar>
      <Avatar sx={{height: 165,width:165}}  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"></Avatar>
             </ProfileAvatar>
+            <Container_Profile>
             <ProfileName>
-            Harry  
+            Harry 
             </ProfileName>
+            <Profile_email>
+            Email:harrykenvic@gmail.com
+            </Profile_email>
+            </Container_Profile>
+           
         </Container>
+       
 
         </div>
     );
