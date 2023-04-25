1736d7278a3f5f30745cc57c9d5e88856f0ccda8
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-04-25 14:09:48 +0530

    added server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 //https://podstream.onrender.com/api
-const API = axios.create({ baseURL: `http://localhost:8700/api` }); 
+const API = axios.create({ baseURL: `https://podstream.onrender.com/api` }); 
 
 //auth
 export const signIn = async ({ email, password }) => await API.post('/auth/signin', { email, password });
